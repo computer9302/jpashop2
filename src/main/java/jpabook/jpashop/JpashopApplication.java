01f7d5682e9d8f171d2d52b56dfeb88368e6fbc8
computer9302 <computer9302@gmail.com>
2023-05-18 11:57:00 +0900

    실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발(프로젝트 생성)

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -8,7 +8,7 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 		Hello hello = new Hello();
-		hello.setData("dsfdd");
+		hello.setData("dsfzdd");
 
 		String data = hello.getData();
 		System.out.println("data = " + data);
