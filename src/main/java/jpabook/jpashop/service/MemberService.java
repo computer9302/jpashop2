895eaf4a32552eb29746842fdf19640b6c0ad708
computer9302 <computer9302@gmail.com>
2023-05-30 15:39:58 +0900

    실전! 스프링 부트와 JPA 활용1 - 웹 애플리케이션 개발(회원 기능 테스트)

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -30,8 +30,8 @@ public class MemberService {
 
     private void validateDuplicateMember(Member member) {
         List<Member> findMembers = memberRepository.findByName(member.getName());
-        if(findMembers.isEmpty()){
-            throw new IllegalStateException("이미 존재하는 회원입니다");
+        if(!findMembers.isEmpty()){
+            throw new IllegalStateException("이미 존재 하는 회원 입니다");
         }
     }
 
